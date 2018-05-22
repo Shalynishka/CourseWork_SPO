ca4317f476116d5a025e430b9889bf26f8aada6f
Shalynishka <36746354+Shalynishka@users.noreply.github.com>
2018-05-22 21:09:59 +0300

    обновлена структура

--- Handler/adminHandler.py
+++ Handler/adminHandler.py
@@ -1,14 +1,20 @@
+# изменино 21.05.18
 from Handler.FileHandler import get_user_list
+from Handler.FileHandler import delete_user
 import vkapi
 
 host = '153466186'
 
 
 def choose_command(data, token):
-    if '/spam' in data['body']:
-        spam(data['body'][6:], token)
-    elif '/users' in data['body']:
+    if '//spam' in data:
+        spam(data[6:], token)
+    elif '//users' in data:
         show_users(token)
+    else:
+        attachment = ''
+        mess = 'Команда не определена: ' + data
+        vkapi.send_messages(host, token, mess, attachment)
 
 
 def spam(mes_to_spam, token):
@@ -19,18 +25,29 @@ def spam(mes_to_spam, token):
         if user_id0 == host:
             continue
         else:
-            vkapi.send_messages(user_id0, token, mes_to_spam, attachment)
+            try:
+                vkapi.send_messages(user_id0, token, mes_to_spam, attachment)
+            except Exception as ex:
+                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                wrong = template.format(type(ex).__name__, ex.args)
+                message = '\n' + 'Ошибка спама:\n' + 'vk.com/id' + str(user_id0) + '\nзабанил нас\n'  + wrong
+                delete_user(user_id0)
+                vkapi.send_messages(host, token, message, attachment)
+    vkapi.send_messages(host, token, 'Сделано', attachment)
+
+
 
 
 def show_users(token):
     attachment = ''
     list_to_spam = get_user_list()
     message = 'Users:\n'
-
+    i = 1
     for l in list_to_spam:
         user_id0 = l
         if user_id0 == host:
             continue
         else:
-            message = message + 'vk.com/id' + str(l) + '\n'
-    vkapi.send_messages(user_id0, token, message, attachment)
+            message =message + str(i) + ': ' +  'vk.com/id' + str(l) + '\n'
+            i += 1
+    vkapi.send_messages(host, token, message, attachment)
