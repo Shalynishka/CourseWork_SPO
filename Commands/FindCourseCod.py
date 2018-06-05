e36d315a7a31d2de1cecd99c2de9eb04d981b23e
Shalynishka <36746354+Shalynishka@users.noreply.github.com>
2018-06-05 21:45:33 +0300

    Update FindCourseCod.py

--- Commands/FindCourseCod.py
+++ Commands/FindCourseCod.py
@@ -1,45 +1,46 @@
-import requests
-from Handler import CommandHandler
+# import requests
+# from Handler import CommandHandler
 
-url_name = 'http://www.nbrb.by/API/ExRates/Rates/{}?ParamMode=2'
-# описание возвращаемого объекта json:{
-# "Cur_ID":145,
-# "Date":"2018-05-22T00:00:00",
-# "Cur_Abbreviation":"USD",
-# "Cur_Scale":1,
-# "Cur_Name":"Доллар США",
-# "Cur_OfficialRate":2.0022}
-
-
-def get_course_with_name(name):
-    try:
-        usd_course = requests.get(url_name.format(name.upper())).json()
-        return str(usd_course['Cur_Scale']) + ' ' + usd_course['Cur_Name'] + ' - ' + str(usd_course['Cur_OfficialRate']) + ' белорусских рубля'
-    except:
-        return 'По данному запросу возникла ошибка. Проверьте правильность написания короткого имени'
+# url_name = 'http://www.nbrb.by/API/ExRates/Rates/{}?ParamMode=2'
+# # описание возвращаемого объекта json:{
+# # "Cur_ID":145,
+# интегрирован в курс
+# # "Date":"2018-05-22T00:00:00",
+# # "Cur_Abbreviation":"USD",
+# # "Cur_Scale":1,
+# # "Cur_Name":"Доллар США",
+# # "Cur_OfficialRate":2.0022}
 
 
-def course_pro(name):
-    message = ''
-    message += get_course_with_name(name)
-    return message, ''
+# def get_course_with_name(name):
+#     try:
+#         usd_course = requests.get(url_name.format(name.upper())).json()
+#         return str(usd_course['Cur_Scale']) + ' ' + usd_course['Cur_Name'] + ' - ' + str(usd_course['Cur_OfficialRate']) + ' белорусских рубля'
+#     except:
+#         return 'По данному запросу возникла ошибка. Проверьте правильность написания короткого имени'
 
 
-info_command = CommandHandler.Command()
-info_command.keys = ['/:']
-info_command.description = 'Покажу курс указанной валюты. Формат команды /:[имя_валюты](usd, EUR и т.д.)'
-info_command.process = course_pro
+# def course_pro(name):
+#     message = ''
+#     message += get_course_with_name(name)
+#     return message, ''
 
 
-def func(*args):
-    if '/stre' in args[0]:
-        print(True)
-    else:
-        print('false')
+# info_command = CommandHandler.Command()
+# info_command.keys = ['/:']
+# info_command.description = 'Покажу курс указанной валюты. Формат команды /:[имя_валюты](usd, EUR и т.д.)'
+# info_command.process = course_pro
 
 
-st = "/:usd"
-print(st[2:])
-func(st)
-func('headf/:')
-func('/:usd')
+# def func(*args):
+#     if '/stre' in args[0]:
+#         print(True)
+#     else:
+#         print('false')
+
+
+# st = "/:usd"
+# print(st[2:])
+# func(st)
+# func('headf/:')
+# func('/:usd')
