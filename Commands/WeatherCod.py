c1c43c3df5a6f02e8b8b385470873f6c874e4979
Shalynishka <36746354+Shalynishka@users.noreply.github.com>
2018-06-05 21:48:45 +0300

    Update WeatherCod.py

--- Commands/WeatherCod.py
+++ Commands/WeatherCod.py
@@ -1,34 +1,39 @@
-import requests
+import re
 import json
-from Handler import CommandHandler
+import requests
+from Handler import command_handler
 
 api_key = 'e0c8b55b1c3db7832226c7b6dfae20fc'
 
 url = 'http://api.openweathermap.org/data/2.5/find?q={}&type=like&units=metric&lang=ru&APPID=e0c8b55b1c3db7832226c7b6dfae20fc'
 
 
-def get_weather(*args):
-    city = args[0][9:]
-    try:
-        #data = requests.get("http://api.openweathermap.org/data/2.5/find", params={'id': city, 'units': 'metric', 'lang': 'ru', 'APPID': api_key}).json()
-        data = requests.get(url.format(city)).json()
-        with open('answer.json', 'w') as f:
-            json.dump(data, f, indent=2, ensure_ascii=False)
-        if data['count'] == 0:
-            return 'Нет совпадений по городу {}'.format(city), ''
-        data_list = list(data['list'])
-        temp = data_list[0]['main']['temp']
-        pressure = data_list[0]['main']['pressure']
-        humidity = data_list[0]['main']['humidity']
-        # country = data['list'][0]['sys']['country']
-        weather = data_list[0]['weather'][0]['description']
-        return 'Температура:{}\nДавление:{}\nВлажность:{}\n{}'.format(str(temp), str(pressure), str(humidity), weather), ''
-    except Exception as e:
-        return "Exception (find):{}".format(str(e)), ''
-        pass
+class Weather:
+    def __init__(self):
+        weather_command = command_handler.Command()
+        weather_command.keys = ['/weather']
+        weather_command.description = 'Покажу погоду указанного города. (/weather минск)'
+        weather_command.process = Weather.get_weather
+
+    @staticmethod
+    def get_weather(*args):
+        city = re.findall('/\w+\s+\S+', args[0])[0][9:]
+        try:
+            #data = requests.get("http://api.openweathermap.org/data/2.5/find", params={'id': city, 'units': 'metric', 'lang': 'ru', 'APPID': api_key}).json()
+            data = requests.get(url.format(city)).json()
+            with open('answer.json', 'w') as f:
+                json.dump(data, f, indent=2, ensure_ascii=False)
+            if data['count'] == 0:
+                return 'Нет совпадений по городу {}'.format(city), ''
+            data_list = list(data['list'])
+            temp = data_list[0]['main']['temp']
+            pressure = data_list[0]['main']['pressure']
+            humidity = data_list[0]['main']['humidity']
+            # country = data['list'][0]['sys']['country']
+            weather = data_list[0]['weather'][0]['description']
+            return 'Температура:{}\nДавление:{}\nВлажность:{}\n{}'.format(str(temp), str(pressure), str(humidity), weather), ''
+        except Exception as e:
+            return "Exception (find):{}".format(str(e)), ''
 
 
-weather_command = CommandHandler.Command()
-weather_command.keys = ['/weather']
-weather_command.description = 'Покажу погоду указанного города. (/weather минск)'
-weather_command.process = get_weather
+Weather()
