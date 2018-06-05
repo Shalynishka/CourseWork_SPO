341969f48992a83062cf66231dbb53357b8d7166
Shalynishka <36746354+Shalynishka@users.noreply.github.com>
2018-06-05 21:46:33 +0300

    Update GameCod.py

--- Commands/GameCod.py
+++ Commands/GameCod.py
@@ -1,4 +1,4 @@
-from Handler import CommandHandler
+from Handler import command_handler
 import requests
 import random
 
@@ -23,6 +23,12 @@ gamers = dict()
 
 
 class GameChecker:
+    def __init__(self):
+        game_command = command_handler.Command()
+        game_command.keys = ['/game']
+        game_command.description = 'Поиграем в слова'
+        game_command.process = GameChecker.game
+
     def __init__(self, user_id):
         self.__user_id = user_id
         self.__useless_words = list()
@@ -38,25 +44,27 @@ class GameChecker:
         self.__user_id = user_id
 
     # @property
-    def get_result(self):
+    @property
+    def result(self):
         return self.__result
-
-    # покажет использованные слова
-    def show_useless(self):
-        print(self.__useless_words)
-        pass
 
     # увеличивает результат
     def up_result(self):
         self.__result += 1
         pass
 
+    # # покажет использованные слова
+    # def show_useless(self):
+    #     print(self.__useless_words)
+    #     pass
+
     # добавляет использованное слово
     def add_useless(self, word):
         self.__useless_words.append(word)
 
     # проверка слова на правильность и существование
-    def check_word_ex(self, s):
+    @staticmethod
+    def check_word_ex(s):
         answer = requests.get(word_check_url.format(s)).json()
         if answer == list():
             return 'ok'
@@ -89,7 +97,8 @@ class GameChecker:
             return 'ok'
 
     # вернуть последнюю букву для следующего слова бота
-    def get_last_letter(self, word):
+    @staticmethod
+    def get_last_letter(word):
         word_ls = list(word)
         word_ls.reverse()
         for l in word_ls:
@@ -100,82 +109,80 @@ class GameChecker:
                 return 'е'
             return l
 
+    @staticmethod
+    def check_user(user_id):
+        if user_id in gamers:
+            return 'ok'
+        else:
+            return 'bad'
 
-def game(*args):
-    body = args[0]
-    user_id = args[1]
-    if user_id not in gamers:
-        gamers[user_id] = GameChecker(user_id)
-        return 'Правила: пиши существительные. Хотя я плохо знаю части речи, но учти: я проверю тебя на вшивость. ' \
-               'Ошибка в слове карается поражением. ' \
-               'Повторение слова тоже карается поражением. ' \
-               'Буква ё будет считаться за е\nЕсли захочешь закончить игру, напиши end\n' + answers_for_start[random.randint(1, 3)], ''
-    else:
-        g = gamers[user_id]
-        # игрок захотел завершить игру
-        if 'end' in body:
-            res = answers_for_end[random.randint(1, 2)].format(str(g.get_result()))
-            del g
-            gamers.pop(user_id)
-            return res, ''
-        # игрок ввел неправильное или несуществующее слово
-        if g.check_word_ex(body) == 'bad':
-            res = 'В слове ошибка! Ну или ты просто слова выдумываешь\n' + answers_for_lose[random.randint(1, 2)]. \
-                format(str(g.get_result()))
-            del g
-            gamers.pop(user_id)
-            return res, ''
-        # игрок ввел английские слова
-        if g.check_last_letter(body[0]) == 'eng':
-            res = 'Э, не, только русские буквы!\n' + answers_for_lose[random.randint(1, 2)].format(str(g.get_result()))
-            del g
-            gamers.pop(user_id)
-            return res, ''
-        # игрок ввел слово не на ту букву
-        if g.check_last_letter(body[0]) == 'bad':
-            res = 'Не та буква!\n' + answers_for_lose[random.randint(1, 2)].format(str(g.get_result()))
-            del g
-            gamers.pop(user_id)
-            return res, ''
-        # игрок ввел слово, которое уже было
-        if g.check_useless_word(body) == 'bad':
-            res = 'Такое слово уже было!' + answers_for_lose[random.randint(1, 2)].format(str(g.get_result()))
-            del g
-            gamers.pop(user_id)
-            return res, ''
-        # запомнили это слово
-        g.add_useless(body)
-        last_let = g.get_last_letter(body)
-        words = list()
-        with open(file_way + last_let + '.txt', 'rt') as f:
-            for line in f:
-                words.append(line)
-            # если после 5-ой попытки бот не найдет подходящее слово, то он проиграл
-            i = 5
-            length = words.__len__()
-            while i > 0:
-                try_word = words[random.randint(0, length - 1)][:-1]
+    @staticmethod
+    def game(*args):
+        # body = args[0]
+        # user_id = args[1]
+        if args[1] not in gamers:
+            gamers[args[1]] = GameChecker(args[1])
+            return 'Правила: пиши существительные. Хотя я плохо знаю части речи, но учти: я проверю тебя на вшивость. ' \
+                   'Ошибка в слове карается поражением. ' \
+                   'Повторение слова тоже карается поражением. ' \
+                   'Буква ё будет считаться за е\nЕсли захочешь закончить игру, напиши end\n' + answers_for_start[random.randint(1, 3)], ''
+        else:
+            g = gamers[args[1]]
+            # игрок захотел завершить игру
+            if args[0] == 'end':
+                res = answers_for_end[random.randint(1, 2)].format(str(g.result))
+                # del g
+                gamers.pop(args[1])
+                return res, ''
+            # игрок ввел неправильное или несуществующее слово
+            if g.check_word_ex(args[0]) == 'bad':
+                res = 'В слове ошибка! Ну или ты просто слова выдумываешь\n' + answers_for_lose[random.randint(1, 2)]. \
+                    format(str(g.get_result()))
+                # del g
+                gamers.pop(args[1])
+                return res, ''
+            check = g.check_last_letter(args[0][0])
+            # игрок ввел английские слова
+            if check == 'eng':
+                res = 'Э, не, только русские буквы!\n' + answers_for_lose[random.randint(1, 2)].format(str(g.get_result()))
+                # del g
+                gamers.pop(args[1])
+                return res, ''
+            # игрок ввел слово не на ту букву
+            elif check == 'bad':
+                res = 'Не та буква!\n' + answers_for_lose[random.randint(1, 2)].format(str(g.get_result()))
+                # del g
+                gamers.pop(args[1])
+                return res, ''
+            # игрок ввел слово, которое уже было
+            if g.check_useless_word(args[0]) == 'bad':
+                res = 'Такое слово уже было!' + answers_for_lose[random.randint(1, 2)].format(str(g.get_result()))
+                # del g
+                gamers.pop(args[1])
+                return res, ''
+            # запомнили это слово
+            g.add_useless(args[0])
+            last_let = g.get_last_letter(args[0])
+            words = list()
+            with open(file_way + last_let + '.txt', 'rt') as f:
+                for line in f:
+                    words.append(line)
+                # если после 5-ой попытки бот не найдет подходящее слово, то он проиграл
+                i = 5
+                length = words.__len__()
+                while i > 0:
+                    try_word = words[random.randint(0, length - 1)][:-1]
 
-                if g.check_useless_word(try_word) == 'bad':
-                    i -= 1
-                else:
-                    g.add_useless(try_word)
-                    g.up_result()
-                    return try_word, ''
-        f.close()
-        del g
-        gamers.pop(user_id)
-        return answers_for_win[random.randint(1, 2)].format(str(g.get_result())), ''
-
-
-game_command = CommandHandler.Command()
-game_command.keys = ['/game']
-game_command.description = 'Поиграем в слова'
-game_command.process = game
+                    if g.check_useless_word(try_word) == 'bad':
+                        i -= 1
+                    else:
+                        g.add_useless(try_word)
+                        g.up_result()
+                        return try_word, ''
+            # f.close()
+            # del g
+            gamers.pop(args[1])
+            return answers_for_win[random.randint(1, 2)].format(str(g.get_result())), ''
 
 
-def check_user(user_id):
-    if user_id in gamers:
-        return 'ok'
-    else:
-        return 'bad'
+          GameChecker()
