df0a0950a1130fd18f87e65382cca8b54667d689
Shalynishka <36746354+Shalynishka@users.noreply.github.com>
2018-06-05 21:46:49 +0300

    Update HelloCo-d.py

--- Commands/HelloCo-d.py
+++ Commands/HelloCo-d.py
@@ -1,12 +1,20 @@
-from Handler import CommandHandler
+from Handler import command_handler
+import random
+helloAnswer = {1: 'Hello', 2: 'Привет', 3: 'Здорово!', 4:  'Доброго времени суток!', 5: 'Хай', 6: 'Хаюшки', 7: 'Мое почтение', 8: 'Здря', 9: 'Дороу', 10: 'Hi'}
 
 
-def hello():
-    message = 'Hello, i\'m bot\n Do u now de way?'
-    return message, ''
+class Hello:
+    def __init__(self):
+        hello_command = command_handler.Command()
+        hello_command.keys = ['привет', 'день добрый!', 'хай', 'вечер в хату', 'дороу', 'hello', 'хаюшки']  # 6
+        hello_command.description = 'Поздороваюсь'
+        hello_command.process = Hello.hello
+
+    @staticmethod
+    def hello(*args):
+        message = helloAnswer.get(random.randint(1,10))
+        return message, ''
 
 
-hello_command = CommandHandler.Command()
-hello_command.keys = ['привет', 'день добрый!', 'хай', 'вечер в хату', 'дороу', 'hello'] #6
-hello_command.description = 'Поздороваюсь'
-hello_command.process = hello
+Hello()
+
