f06b4dcf1014103064c0d601417de69f97a02480
Shalynishka <36746354+Shalynishka@users.noreply.github.com>
2018-06-05 21:44:39 +0300

    Update CourseCod.py

--- Commands/CourseCod.py
+++ Commands/CourseCod.py
@@ -1,14 +1,17 @@
 import requests
-from Handler import CommandHandler
+import re
+from Handler import command_handler
 
 classic = [145, 292, 298, 293, 143]
-usd = 145
-rus = 298
-eur = 292
-zlot = 293
-funt_en = 143
+# usd = 145
+# rus = 298
+# eur = 292
+# zlot = 293
+# funt_en = 143
 
 url_num = 'http://www.nbrb.by/API/ExRates/Rates/'
+url_name = 'http://www.nbrb.by/API/ExRates/Rates/{}?ParamMode=2'
+
 # описание возвращаемого объекта json:{
 #  'Cur_ID': 145,                   внутренний код валюты
 #  'Date': '2018-05-22T00:00:00',   дата, на которую запрашивается курс
@@ -18,25 +21,54 @@ url_num = 'http://www.nbrb.by/API/ExRates/Rates/'
 #  'Cur_OfficialRate': 2.0022}      курс
 
 
-def get_course_with_num(num):
-    try:
-        usd_course = requests.get(url_num + str(num)).json()
-        return str(usd_course['Cur_Scale']) + ' ' + usd_course['Cur_Name'] + ' - ' + str(usd_course['Cur_OfficialRate']) + ' белорусских рубля'
-    except:
-        return 'По данному запросу возникла ошибка. Не подвезли курс?'
+class Course:
+    def __init__(self):
+        info_command = command_handler.Command()
+        info_command.keys = ['/course']
+        info_command.description = 'Покажу курс валют'
+        info_command.process = Course.course_classic
+
+        info_command = command_handler.Command()
+        info_command.keys = ['/:']
+        info_command.description = 'Покажу курс указанной валюты. Формат команды /:[имя_валюты](usd, EUR и т.д.)'
+        info_command.process = Course.course_pro
+
+    @staticmethod
+    def get_course_with_num(num):
+        try:
+            usd_course = requests.get(url_num + str(num)).json()
+            return str(usd_course['Cur_Scale']) + ' ' + usd_course['Cur_Name'] + ' - ' + str(usd_course['Cur_OfficialRate']) + ' белорусских рубля'
+        except:
+            return 'По данному запросу возникла ошибка. Не подвезли курс?'
+
+    @staticmethod
+    def course_classic(*args):
+        message = ''
+        for c in classic:
+            message += Course.get_course_with_num(c)
+        return message, ''
+
+    @staticmethod
+    def get_course_with_name(name):
+        try:
+            usd_course = requests.get(url_name.format(name.upper())).json()
+            return str(usd_course['Cur_Scale']) + ' ' + usd_course['Cur_Name'] + ' - ' + str(
+                usd_course['Cur_OfficialRate']) + ' белорусских рубля\n'
+        except:
+            return 'По данному запросу возникла ошибка. Проверьте правильность написания короткого имени\n'
+
+    @staticmethod
+    def course_pro(*args):
+        message = ''
+        pattern = re.compile('/:\w+')
+        ind = 0
+        for c in pattern.findall(args[0]):
+            ind += 1
+            if c.__len__() != 5:
+                message += 'Неверный формат имени с индексом {}. Короткие имена валют состоят из трех символов\n'.format(str(ind))
+            else:
+                message += Course.get_course_with_name(c[2:])
+        return message, ''
 
 
-def course_classic(*args):
-    message = ''
-    for c in classic:
-        message += get_course_with_num(c)
-    return message, ''
-
-
-info_command = CommandHandler.Command()
-info_command.keys = ['/course']
-info_command.description = 'Покажу курс валют'
-info_command.process = course_classic
-
-
-course_classic()
+Course()
