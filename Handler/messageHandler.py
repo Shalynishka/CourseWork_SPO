80c39dc1d4eb2b9d55cebabc3996ab84dff6ae74
Shalynishka <36746354+Shalynishka@users.noreply.github.com>
2018-06-05 21:54:13 +0300

    Массовое обновление структуры
    Обертка в классы

--- Handler/messageHandler.py
+++ Handler/messageHandler.py
@@ -1,57 +1,57 @@
-#принимает сообщение пользователя, обрабатывает его
-#changed 21.05.18, изменена обработка ошибки с файлом. Organizer
-import vkapi
+# принимает сообщение пользователя, обрабатывает его
+# changed 21.05.18, изменена обработка ошибки с файлом. Organizer
+import vk_api
 import os
 import random
 import importlib
-from Handler.CommandHandler import command_list
-from Handler.FileHandler import add_user
-from Handler.adminHandler import choose_command
-host = 153466186
+from Handler.command_handler import command_list
+from Handler.file_handler import FileWork
+from Handler.admin_handler import Admin
+host = '153466186'
 problemMessage = {1: 'Прости, не понимаю тебя.', 2: 'Сорян, не разбираю твои каракули', 3: 'Трудности перевода, воспользуйся помощью', 4: 'Я не ИИ, пиши по ГОСТу'}
 
 
-# парсим сообщение и выбираем, что сделать
-def get_answer(body, user_id):
-    message = problemMessage.get(random.randint(1, 4))
-    attachment = ''
-    # необходимо сделать файл, тогда разграничим две функции
-    for c in command_list:
-        for d in c.keys:
-            if d in body:
+class Answer:
+    # парсим сообщение и выбираем, что сделать
+    @staticmethod
+    def get_answer(body, user_id):
+        message = problemMessage.get(random.randint(1, 4))
+        attachment = ''
+        # необходимо сделать файл, тогда разграничим две функции
+        for c in command_list:
+            if body in c.keys:
                 message, attachment = c.process(body, user_id)
-        #if body in c.keys:
-    return message, attachment
+        return message, attachment
 
-
-# получили сообщение и делаем штуки с ним
-def create_answer(data, token):
-    # загрузить модули
-    load_modules()
-    # добавить пользователя
-    user_id = data['user_id']
-    try:
-        add_user(user_id)
-    except Exception as ex:
-        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-        wrong = template.format(type(ex).__name__, ex.args)
-        error_message = 'Ошибка добавления пользователя:\n' + wrong
-        vkapi.send_messages(host, token, error_message, '')
-    finally:
-        if '/' == data['body'].lower()[1]:
-            if user_id != host:
-                vkapi.send_messages(user_id, token, 'Нет доступа к командам', '')
+    # получили сообщение и делаем штуки с ним
+    @staticmethod
+    def create_answer(data, token):
+        # загрузить модули
+        Answer.load_modules()
+        # добавить пользователя
+        user_id = data['user_id']
+        try:
+            FileWork.add_user(user_id)
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            wrong = template.format(type(ex).__name__, ex.args)
+            error_message = 'Ошибка добавления пользователя:\n' + wrong
+            vk_api.send_messages(host, token, error_message, '')
+        finally:
+            if '/' == data['body'][1]:
+                if user_id != host:
+                    vk_api.send_messages(user_id, token, 'Нет доступа к командам', '')
+                else:
+                    Admin.choose_command(data['body'], token)
+                # data['body'] = 'wat'
             else:
-                choose_command(data['body'], token)
-            data['body'] = 'wat'
-        else:
-            message, attachment = get_answer(data['body'].lower(), user_id)
-            vkapi.send_messages(user_id, token, message, attachment)
+                message, attachment = Answer.get_answer(data['body'].lower(), user_id)
+                vk_api.send_messages(user_id, token, message, attachment)
 
-
-def load_modules():
-    files = os.listdir("mysite/Commands")
-    modules = filter(lambda x: x.endswith('.py'), files)
-    for m in modules:
-        importlib.import_module("Commands." + m[0:-3])
+    @staticmethod
+    def load_modules():
+        files = os.listdir("mysite/Commands")
+        modules = filter(lambda x: x.endswith('.py'), files)
+        for m in modules:
+            importlib.import_module("Commands." + m[0:-3])
 
